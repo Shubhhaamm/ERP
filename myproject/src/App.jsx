7ae8c2fe6fba580cb7e16ecd9120c193c8ecd96c
Shubham Bagul <shubham2435bagul@gmail.com>
2025-03-25 15:00:22 +0530

    Initial commit

---
+++ myproject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-500'>
+        Lorem ipsum dolor sit amet consectetur.
+
+      </h1>
+    </div>
+  )
+}
+
+export default App
